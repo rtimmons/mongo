b8d4444bca42e50921fe01887914659004e9eebc
Ryan Timmons <ryan.timmons@mongodb.com>
2019-09-12 14:25:15 -0400

    lint

--- jstests/resmoke_selftest/peer_pids.js
+++ jstests/resmoke_selftest/peer_pids.js
@@ -4,11 +4,10 @@
  * This tests the resmoke functionality of passing peer pids to TestData.
  */
 
-(function(){
+(function() {
 
 assert(typeof TestData.peerPids !== 'undefined');
 
 // ShardedClusterFixture 2 shards with 3 rs members per shard, 2 mongos's => 7 peers
 assert.eq(7, TestData.peerPids.length);
-
 })();
