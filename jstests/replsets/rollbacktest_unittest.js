5079e3bcda0abd13e2b3dcef12baa2987edcee9b
Ryan Timmons <ryan.timmons@mongodb.com>
2020-01-15 14:14:04 -0500

    clang-format

--- jstests/replsets/rollbacktest_unittest.js
+++ jstests/replsets/rollbacktest_unittest.js
@@ -6,72 +6,71 @@
 load("jstests/replsets/rslib.js");
 load("jstests/replsets/libs/rollback_test.js");
 
-(function(){
+(function() {
 "use strict";
 
-    // callback is type f: RollbackTest -> void
-    const testCase = function(callback) {
-        const context = {
-            checkDataConsistencyCallCount: 0,
-            stopSetCallCount: 0,
-        };
-
-        const rollbackTest = new RollbackTest("rollbacktest_unittest");
-        rollbackTest._checkDataConsistencyImpl = function() {
-            ++context.checkDataConsistencyCallCount;
-        };
-
-        const rst = rollbackTest.getTestFixture();
-        rst.stopSet = function(signal, forRestart, opts) {
-            // Unoconditionally skip in rst.stopSet because rbt.stop does its own validation.
-            assert.eq(opts, {"skipCheckDBHashes" : true, "skipValidation" : true});
-            ++context.stopSetCallCount;
-
-            // We don't care about doing the regular stopSet actions.
-            for(let i=rst.nodeList().length-1; i >= 0; --i) {
-                rst.stop(i);
-            }
-        };
-
-        callback(rollbackTest, context);
+// callback is type f: RollbackTest -> void
+const testCase = function(callback) {
+    const context = {
+        checkDataConsistencyCallCount: 0,
+        stopSetCallCount: 0,
     };
 
-    // Each call to testCase takes about 10 seconds, so be judicious about
-    // adding too many of them.
+    const rollbackTest = new RollbackTest("rollbacktest_unittest");
+    rollbackTest._checkDataConsistencyImpl = function() {
+        ++context.checkDataConsistencyCallCount;
+    };
 
-    testCase((rbt, ctx) => {
-        assert.eq(ctx.checkDataConsistencyCallCount, 0);
-        assert.eq(ctx.stopSetCallCount, 0);
-        rbt.stop();
-        assert.eq(ctx.checkDataConsistencyCallCount, 1);
-        assert.eq(ctx.stopSetCallCount, 1);
-    });
+    const rst = rollbackTest.getTestFixture();
+    rst.stopSet = function(signal, forRestart, opts) {
+        // Unoconditionally skip in rst.stopSet because rbt.stop does its own validation.
+        assert.eq(opts, {"skipCheckDBHashes": true, "skipValidation": true});
+        ++context.stopSetCallCount;
 
-    testCase((rbt, ctx) => {
-        // Force it.
-        rbt.checkDataConsistency();
-        // Above test asserts that the call counts are 1 now.
+        // We don't care about doing the regular stopSet actions.
+        for (let i = rst.nodeList().length - 1; i >= 0; --i) {
+            rst.stop(i);
+        }
+    };
 
-        // We haven't done anything that should require more changes.
-        rbt.stop();
-        assert.eq(ctx.checkDataConsistencyCallCount, 1);
-        assert.eq(ctx.stopSetCallCount, 1);
-    });
+    callback(rollbackTest, context);
+};
 
-    testCase((rbt, ctx) => {
-        // Force it.
-        rbt.checkDataConsistency();
+// Each call to testCase takes about 10 seconds, so be judicious about
+// adding too many of them.
 
-        // State-transitions forces an additional consistency-check.
-        rbt.transitionToRollbackOperations();
-        rbt.transitionToSyncSourceOperationsBeforeRollback();
-        rbt.transitionToSyncSourceOperationsDuringRollback();
-        rbt.transitionToSteadyStateOperations();
+testCase((rbt, ctx) => {
+    assert.eq(ctx.checkDataConsistencyCallCount, 0);
+    assert.eq(ctx.stopSetCallCount, 0);
+    rbt.stop();
+    assert.eq(ctx.checkDataConsistencyCallCount, 1);
+    assert.eq(ctx.stopSetCallCount, 1);
+});
 
-        // This will do another call.
-        rbt.stop();
-        assert.eq(ctx.checkDataConsistencyCallCount, 2);
-        assert.eq(ctx.stopSetCallCount, 1);
-    });
+testCase((rbt, ctx) => {
+    // Force it.
+    rbt.checkDataConsistency();
+    // Above test asserts that the call counts are 1 now.
 
+    // We haven't done anything that should require more changes.
+    rbt.stop();
+    assert.eq(ctx.checkDataConsistencyCallCount, 1);
+    assert.eq(ctx.stopSetCallCount, 1);
+});
+
+testCase((rbt, ctx) => {
+    // Force it.
+    rbt.checkDataConsistency();
+
+    // State-transitions forces an additional consistency-check.
+    rbt.transitionToRollbackOperations();
+    rbt.transitionToSyncSourceOperationsBeforeRollback();
+    rbt.transitionToSyncSourceOperationsDuringRollback();
+    rbt.transitionToSteadyStateOperations();
+
+    // This will do another call.
+    rbt.stop();
+    assert.eq(ctx.checkDataConsistencyCallCount, 2);
+    assert.eq(ctx.stopSetCallCount, 1);
+});
 })();
