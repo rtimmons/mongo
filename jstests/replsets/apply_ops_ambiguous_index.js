fa1bbd738f6ee71558a594338d1ccd002cf0eafd
Ryan Timmons <ryan.timmons@10gen.com>
2020-01-24 15:17:13 -0500

    try approach 2 - still no luck

--- jstests/replsets/apply_ops_ambiguous_index.js
+++ jstests/replsets/apply_ops_ambiguous_index.js
@@ -8,11 +8,14 @@ var rst = new ReplSetTest({name: "initial_sync_move_forward", nodes: 1});
 rst.startSet();
 rst.initiate();
 
+
 var masterColl = rst.getPrimary().getDB("test").coll;
 
-// Insert _id={0,1}
+// Insert _id={0,1,2}
+masterColl.ensureIndex({"a.0": 1}); // (1)
 masterColl.insertOne({_id: 0, a:0});
 masterColl.insertOne({_id: 1, a:1});
+masterColl.insertOne({_id: 2, a:2});
 
 // Add a secondary.
 var secondary = rst.add({setParameter: "numInitialSyncAttempts=1"});
@@ -22,13 +25,17 @@ var secondaryColl = secondary.getDB("test").coll;
 
 var failPoint = configureFailPoint(secondary,
                                    "initialSyncHangDuringCollectionClone",
-                                   {namespace: secondaryColl.getFullName(), numDocsToClone: 2});
+                                   {namespace: secondaryColl.getFullName(), numDocsToClone: 4});
 rst.reInitiate();
-failPoint.wait();
+failPoint.wait(10000);
 
-masterColl.insertOne({_id: 2, a:[{"0": 1}]});
-masterColl.deleteOne({_id: 2});
-masterColl.ensureIndex({"a.0": 1});
+
+// masterColl.insertOne({_id: 2, a:[{"0": 1}]}); (2)
+// masterColl.deleteOne({_id: 2});
+// masterColl.ensureIndex({"a.0": 1});
+
+masterColl.dropIndex({"a.0": 1}); // (1)
+masterColl.insertOne({_id: 200, a:[{"0": 1}]});
 
 // Resume initial sync.
 failPoint.off();
@@ -37,7 +44,7 @@ failPoint.off();
 rst.awaitSecondaryNodes();
 
 // Check document count on secondary.
-assert.eq(2, secondaryColl.find().itcount());
+assert.eq(4, secondaryColl.find().itcount());
 
 rst.stopSet();
 })();
