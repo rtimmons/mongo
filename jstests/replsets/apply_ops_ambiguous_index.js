6cdb08355fd02bcab780cba9ab499d29b0fdcf9c
Ryan Timmons <ryan.timmons@10gen.com>
2020-01-28 12:57:47 -0500

    doc updates and var renames per CR feedback

--- jstests/replsets/apply_ops_ambiguous_index.js
+++ jstests/replsets/apply_ops_ambiguous_index.js
@@ -1,46 +1,60 @@
-//
-// Asserts that inserting a document like `{a:[{"0":1}]}` on the
-// primary doesn't cause initial-sync to fail if there was index
-// on `a.0` at the beginning of initial-sync but the `dropIndex`
-// hasn't yet been applied on the secondary prior to applying the
-// insert oplog entry.
-//
+/**
+ * Asserts that inserting a document like `{a:[{"0":1}]}` on the
+ * primary doesn't cause initial-sync to fail if there was index
+ * on `a.0` at the beginning of initial-sync but the `dropIndex`
+ * hasn't yet been applied on the secondary prior to cloning the
+ * insert oplog entry.
+ */
 
 (function() {
 "use strict";
 
 load("jstests/libs/fail_point_util.js");
 
+const DB_NAME = 'test';
+const COLLECTION_NAME = 'coll';
+
+// How many documents to insert on the primary prior to
+// starting initial-sync.
+const INITIAL_DOCS = 10;
+// Batch-size used for cloning.
+// Used as a fail-point parameter as detailed below.
+const CLONER_BATCH_SIZE = 1;
+
+// Setting INITIAL_DOCS larger than CLONER_BATCH_SIZE causes
+// the fail-point to be hit because we fetch
+// multiple batches in the InitialSyncer.
+
 // Start one-node repl-set.
 const rst = new ReplSetTest({name: "apply_ops_ambiguous_index", nodes: 1});
 rst.startSet();
 rst.initiate();
-const masterColl = rst.getPrimary().getDB("test").coll;
+const primaryColl = rst.getPrimary().getDB(DB_NAME).getCollection(COLLECTION_NAME);
 
-// Insert 10 docs that need to be applied on the secondary.
-for (let i = 0; i < 10; ++i) {
-    masterColl.insertOne({_id: i, a: i});
+// Insert the initial document set.
+for (let i = 0; i < INITIAL_DOCS; ++i) {
+    primaryColl.insertOne({_id: i, a: i});
 }
 
 // Add a secondary.
-const secondary =
-    rst.add({setParameter: {"numInitialSyncAttempts": 1, 'collectionClonerBatchSize': 1}});
+const secondary = rst.add(
+    {setParameter: {"numInitialSyncAttempts": 1, 'collectionClonerBatchSize': CLONER_BATCH_SIZE}});
 secondary.setSlaveOk();
-const secondaryColl = secondary.getDB("test").coll;
+const secondaryColl = secondary.getDB(DB_NAME).getCollection(COLLECTION_NAME);
 
 // We set the collectionClonerBatchSize low above, so we will definitely hit
 // this fail-point and hang after the first batch is applied. While the
-// secondary is hung we apply the problematic document.
+// secondary is hung we clone the problematic document.
 const failPoint = configureFailPoint(secondary,
                                      "initialSyncHangCollectionClonerAfterHandlingBatchResponse",
                                      {nss: secondaryColl.getFullName()});
 rst.reInitiate();
 failPoint.wait();
 
-// Master no longer has the problematic index so the insert succeeds on master.
+// Primary no longer has the problematic index so the insert succeeds on the primary.
 // The collection-cloner will resume when the failpoint is turned off.
-masterColl.dropIndex({"a.0": 1});
-masterColl.insertOne({_id: 200, a: [{"0": 1}]});
+primaryColl.dropIndex({"a.0": 1});
+primaryColl.insertOne({_id: 200, a: [{"0": 1}]});
 
 // Resume initial sync. The "bad" document will be applied; the dropIndex will
 // be applied later.
@@ -50,8 +64,8 @@ failPoint.off();
 rst.awaitSecondaryNodes();
 
 // Check document count on secondary.
-// 10 from the initial `for` loop and the 1 "bad" document.
-assert.eq(11, secondaryColl.find().itcount());
+// INITIAL_DOCS from the initial `for` loop and the 1 "bad" document.
+assert.eq(INITIAL_DOCS + 1, secondaryColl.find().itcount());
 
 rst.stopSet();
 })();
