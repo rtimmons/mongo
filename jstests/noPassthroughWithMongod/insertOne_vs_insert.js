7254bd111edda8fe90425dd35531ecbd5416370d
Ryan Timmons <ryan.timmons@mongodb.com>
2020-06-05 13:15:28 -0400

    basic test

---
+++ jstests/noPassthroughWithMongod/insertOne_vs_insert.js
@@ -0,0 +1,11 @@
+// Test insert and insertOne with write-concerns other than {w:1}.
+
+const coll = db.testInserts;
+
+assert.throws(() => coll.insertOne({a:1}, {w:2}));
+
+const insertResult = coll.insert({a:2}, {w:2});
+assert(insertResult instanceof WriteCommandError);
+assert.eq(insertResult.message, "cannot use 'w' > 1 when a host is not replicated");
+
+assert.eq(coll.count(), 0);
